83eb4cdd6b6621764371bc3b5573a29d614f57b0
Gabriel <gabriel.lamela.ch@gmail.com>
2024-03-18 17:42:14 +0100

    Crear base del proyecto en Spring

---
+++ orkdate/src/test/java/com/odaw2a/orkdate/OrkdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odaw2a.orkdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrkdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
